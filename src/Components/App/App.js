cd633b730e7d5700d67ac81682e768dc2249f5ff
Karen Garcia <saykaren@gmail.com>
2021-07-06 15:51:15 -0600

    Subscription and Contact Form Working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,25 @@
 import "../Styling/App.css";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import React from "react";
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        Hello Subscribe
+        Subscribe for 10% Discount Coupon!
         <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
+        <a
+          // href={process.env.REACT_CONTACT_MAILCHIMP_URL}
+          href={`https://us6.list-manage.com/contact-form?u=546943de41f1c48dde40f2b14&form_id=3f34220521c8db9e6f32a19fcf1f6a8f`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Me
+        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
